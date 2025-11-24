ba86f291201186f0243d314cef9f2b4bc24be494
Panom <p.makuei@alustudent.com>
2025-11-24 18:32:59 +0000

    Adding SQL_more_querires

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- grant access to suer
+SHOW GRANTS FOR 'user_0d_1' @'localhost';
+
+SHOW GRANTS FOR 'user_0d_2' @'localhost';
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- getting record
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+    JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
